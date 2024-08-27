9318336808a2519be85d97ac25f12af02461e363
Alexander Skrock <alexanderskrock@gmx.de>
2024-08-27 11:36:06 +0200

    chore(overlays): extracted overlay logic as hook-like function

--- src/BpmnChart/BpmnChart.tsx
+++ src/BpmnChart/BpmnChart.tsx
@@ -3,70 +3,16 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import type { EventBusEventCallback, ImportDoneEvent } from "bpmn-js/lib/BaseViewer";
 import Viewer from "bpmn-js/lib/NavigatedViewer";
 
-import type { BpmnChartProps, OverlayDefinition } from "./BpmnChart.types";
-import { isOverlayDefinition, isOverlayDefinitionBuilder, isOverlayDefinitionsBuilder } from "./BpmnChart.types";
+import type { BpmnChartProps } from "./BpmnChart.types";
 
-
-import { getCanvas, getElementRegistry, getOverlays} from "./serviceHelpers";
+import { getCanvas } from "./serviceHelpers";
+import useOverlays from "./useOverlays";
 
 const BpmnChart: React.FC<BpmnChartProps> = ({ xml, overlays, onViewerInitialized, onLoadingSuccess, onLoadingError }: BpmnChartProps) => {
     const chartContainerRef = useRef<HTMLDivElement>(null);
     const [bpmnViewer, setBpmnViewer] = useState<Viewer | undefined>();
 
-
-    const initializeOverlays = useCallback(() => {
-        if (!bpmnViewer) {
-            return;
-        }
-
-        const overlayService = getOverlays(bpmnViewer);
-        const elementRegistry = getElementRegistry(bpmnViewer);
-
-        overlayService.clear();
-        console.log("Removed all active overlays.");
-
-
-        const overlayDefinitions: OverlayDefinition[] = [];
-
-        const overlayBuilderEnvironment = {
-            rootElement: () => getCanvas(bpmnViewer).getRootElement(),
-            canvas: () => getCanvas(bpmnViewer),
-        };
-
-        overlays?.forEach(overlay => {
-            if (isOverlayDefinition(overlay)) {
-                overlayDefinitions.push(overlay);
-            } else if (isOverlayDefinitionBuilder(overlay)) {
-                const elementFilter = overlay.elementFilter;
-                const elements = typeof elementFilter === 'string'
-                    ? elementRegistry.filter(element => elementFilter === element.id)
-                    : elementRegistry.filter(elementFilter);
-
-                elements.forEach(element => {
-                    const buildResult = overlay.buildDefinition(element, overlayBuilderEnvironment);
-                    overlayDefinitions.push(buildResult);
-                })
-            } else if (isOverlayDefinitionsBuilder(overlay)) {
-                const elements = overlay.elementFilter
-                    ? elementRegistry.filter(overlay.elementFilter)
-                    : elementRegistry.getAll();
-                
-                const buildResults = overlay.buildDefinitions(elements, overlayBuilderEnvironment);
-                buildResults.forEach(overlayDefinition => {
-                    overlayDefinitions.push(overlayDefinition);
-                });
-            }
-        });
-
-        overlayDefinitions.forEach(({ type, element, config }) => {
-            if (type) {
-                overlayService.add(element, type, config);
-            } else {
-                overlayService.add(element, config);
-            }
-        });
-        console.log(`Registered ${overlayDefinitions.length} overlays.`);
-    }, [bpmnViewer, overlays]);
+    useOverlays({ overlays, diagram: bpmnViewer });
 
     const handleImportDone: EventBusEventCallback<ImportDoneEvent> = useCallback((event: ImportDoneEvent) => {
         const { error, warnings } = event;
@@ -75,14 +21,12 @@ const BpmnChart: React.FC<BpmnChartProps> = ({ xml, overlays, onViewerInitialize
           return onLoadingError?.(error);
         }
 
-        initializeOverlays();
-
         if (bpmnViewer) {
             getCanvas(bpmnViewer).zoom('fit-viewport');
         }
 
         return onLoadingSuccess?.({ warnings });
-    }, [bpmnViewer, initializeOverlays, onLoadingSuccess, onLoadingError]);
+    }, [bpmnViewer, onLoadingSuccess, onLoadingError]);
 
     useEffect(() => {
         if (!bpmnViewer && chartContainerRef.current) {
@@ -101,12 +45,14 @@ const BpmnChart: React.FC<BpmnChartProps> = ({ xml, overlays, onViewerInitialize
     }, [bpmnViewer, onViewerInitialized]);
 
     useEffect(() => {
-        if (!bpmnViewer) {
-            return;
+        if (bpmnViewer) {
+            bpmnViewer.on('import.done', handleImportDone);
         }
-
-        bpmnViewer.off('import.done');
-        bpmnViewer.on('import.done', handleImportDone);
+        return () => {
+            if (bpmnViewer) {
+                bpmnViewer.off('import.done', handleImportDone);
+            }
+        }
     }, [bpmnViewer, handleImportDone]);
 
     useEffect(() => {
---
+++ src/BpmnChart/useOverlays.ts
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useMemo} from "react";
+
+import { isOverlayDefinition,isOverlayDefinitionBuilder, isOverlayDefinitionsBuilder } from "./BpmnChart.types";
+import {
+    getCanvas,
+    getElementRegistry,
+    getEventBus,
+    getOverlays
+} from "./serviceHelpers";
+
+const useOverlays = ({ overlays, diagram }) => {
+    const initializeOverlays = useCallback(() => {
+        if (diagram) {
+            const overlayService = getOverlays(diagram);
+            overlayService.clear();
+            console.log("Removed all active overlays.");
+
+            const elementRegistry = getElementRegistry(diagram);
+
+            const builderEnv = ({
+                rootElement: () => getCanvas(diagram).getRootElement(),
+                canvas: () => getCanvas(diagram),
+            });
+
+            const overlayDefinitions = [];
+            overlays?.forEach(overlay => {
+                if (isOverlayDefinition(overlay)) {
+                    overlayDefinitions.push(overlay);
+                } else if (isOverlayDefinitionBuilder(overlay)) {
+                    const elementFilter = overlay.elementFilter;
+                    const elements = typeof elementFilter === 'string'
+                        ? elementRegistry.filter(element => elementFilter === element.id)
+                        : elementRegistry.filter(elementFilter);
+
+                    elements.forEach(element => {
+                        const buildResult = overlay.buildDefinition(element, builderEnv);
+                        overlayDefinitions.push(buildResult);
+                    })
+                } else if (isOverlayDefinitionsBuilder(overlay)) {
+                    const elements = overlay.elementFilter
+                        ? elementRegistry.filter(overlay.elementFilter)
+                        : elementRegistry.getAll();
+
+                    const buildResults = overlay.buildDefinitions(elements, builderEnv);
+                    buildResults.forEach(overlayDefinition => {
+                        overlayDefinitions.push(overlayDefinition);
+                    });
+                }
+            });
+
+            overlayDefinitions.forEach(({type, element, config}) => {
+                if (type) {
+                    overlayService.add(element, type, config);
+                } else {
+                    overlayService.add(element, config);
+                }
+            });
+            console.log(`Registered ${overlayDefinitions.length} overlays.`);
+        }
+    }, [diagram, overlays]);
+
+    useEffect(() => {
+        if (diagram) {
+            getEventBus(diagram).on('import.done', initializeOverlays);
+        }
+        return () => {
+            if (diagram) {
+                getEventBus(diagram).off('import.done', initializeOverlays);
+            }
+        }
+    }, [diagram, initializeOverlays]);
+
+    useEffect(() => {
+        initializeOverlays();
+    }, [initializeOverlays]);
+};
+
+export default useOverlays;
