bdbcbf33337f87864fbecbaf16e53849c6391f4e
Alexander Skrock <alexanderskrock@gmx.de>
2024-09-15 20:43:39 +0200

    feat(heatmap): additional stories for svg and canvas performance

--- stories/Overlays/Heatmap/Heatmap.stories.tsx
+++ stories/Overlays/Heatmap/Heatmap.stories.tsx
@@ -70,6 +70,116 @@ export const SimpleCanvasHeatmap: Story = {
     },
 };
 
+export const PerformanceSvgHeatmap: Story = {
+    loaders: [
+        async () => ({
+            xml: await (await fetch("heatmap/performance.bpmn")).text(),
+        }),
+    ],
+    args: {
+        overlays: [
+            new Heatmap({
+                renderMode: "svg",
+                values: {
+                    "StartEvent_1": 1,
+                    "Activity_01ueqqc": 15,
+                    "Gateway_1bv3th4": 2,
+                    "Activity_194ca2u": 15,
+                    "Gateway_06owj5x": 2,
+                    "Activity_0t7f06a": 5,
+                    "Gateway_17kutfu": 2,
+                    "Event_1bmjtdk": 20,
+                    "Activity_09hs9tc": 15,
+                    "Activity_1rp6goj": 15,
+                    "Event_1pp4ahj": 1,
+                    "Activity_0fb3qw0": 10,
+                    "Activity_1ajpos3": 10,
+                    "Event_11dltkm": 1,
+                    "Gateway_15hub7d": 2,
+                    "Activity_0vbnglx": 5,
+                    "Event_12jsdh5": 20,
+                    "Activity_1cbo157": 5,
+                    "Activity_02ym7m3": 15,
+                    "Activity_0amfcup": 15,
+                    "Gateway_0gt4tpi": 2,
+                    "Activity_0mfzl3o": 5,
+                    "Gateway_1rns7zt": 2,
+                    "Activity_18nlov2": 15,
+                    "Event_1m1to89": 1,
+                    "Activity_0f8dpb5": 15,
+                    "Activity_1slg2xu": 5,
+                    "Activity_0y6g0m5": 10,
+                    "Event_1u6lwgo": 1,
+                    "Activity_0yfv7pg": 15,
+                    "Event_17ll6ic": 1,
+                    "Activity_1qfvwvq": 5,
+                    "Activity_0ovsll1": 15,
+                    "Activity_0zmb9gj": 5,
+                    "Gateway_0w17eje": 2,
+                    "Event_0kxn5ir": 1,
+                    "Activity_0aesgfx": 15,
+                    "Event_1tydb67": 1,
+                },
+            }),
+        ],
+    },
+};
+
+export const PerformanceCanvasHeatmap: Story = {
+    loaders: [
+        async () => ({
+            xml: await (await fetch("heatmap/performance.bpmn")).text(),
+        }),
+    ],
+    args: {
+        overlays: [
+            new Heatmap({
+                renderMode: "canvas",
+                values: {
+                    "StartEvent_1": 1,
+                    "Activity_01ueqqc": 15,
+                    "Gateway_1bv3th4": 2,
+                    "Activity_194ca2u": 15,
+                    "Gateway_06owj5x": 2,
+                    "Activity_0t7f06a": 5,
+                    "Gateway_17kutfu": 2,
+                    "Event_1bmjtdk": 20,
+                    "Activity_09hs9tc": 15,
+                    "Activity_1rp6goj": 15,
+                    "Event_1pp4ahj": 1,
+                    "Activity_0fb3qw0": 10,
+                    "Activity_1ajpos3": 10,
+                    "Event_11dltkm": 1,
+                    "Gateway_15hub7d": 2,
+                    "Activity_0vbnglx": 5,
+                    "Event_12jsdh5": 20,
+                    "Activity_1cbo157": 5,
+                    "Activity_02ym7m3": 15,
+                    "Activity_0amfcup": 15,
+                    "Gateway_0gt4tpi": 2,
+                    "Activity_0mfzl3o": 5,
+                    "Gateway_1rns7zt": 2,
+                    "Activity_18nlov2": 15,
+                    "Event_1m1to89": 1,
+                    "Activity_0f8dpb5": 15,
+                    "Activity_1slg2xu": 5,
+                    "Activity_0y6g0m5": 10,
+                    "Event_1u6lwgo": 1,
+                    "Activity_0yfv7pg": 15,
+                    "Event_17ll6ic": 1,
+                    "Activity_1qfvwvq": 5,
+                    "Activity_0ovsll1": 15,
+                    "Activity_0zmb9gj": 5,
+                    "Gateway_0w17eje": 2,
+                    "Event_0kxn5ir": 1,
+                    "Activity_0aesgfx": 15,
+                    "Event_1tydb67": 1,
+                },
+            }),
+        ],
+    },
+};
+
 export const HeatmapWithSubprocess: Story = {
     loaders: [
         async () => ({
