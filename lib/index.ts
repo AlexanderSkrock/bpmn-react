e96a0eaffce531aca3665b723259c63d594ceb39
Alexander Skrock <alexanderskrock@gmx.de>
2024-09-19 19:13:28 +0200

    chore(package): cleanup root reexports

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,11 @@
+export * as Diagram from "./Diagram";
 export * as Viewer from "./Viewer";
-export * as Modules from "./Modules";
+export * as Modeler from "./Modeler";
 
-export * as Control from "./Control";
+export * as Modules from "./Modules";
 export * as Overlays from "./Overlays";
+
+export * as Components from "./Components";
+export * as Control from "./Control";
 
 export * as util from "./util";
