6d1b481d9135dac72047f7310128b586c42e50a6
Alexander Skrock <alexanderskrock@gmx.de>
2024-09-19 20:49:56 +0200

    chore(modeler): simplify generic modeler hook implementatio

--- lib/Modeler/useBpmnJsModeler.ts
+++ lib/Modeler/useBpmnJsModeler.ts
@@ -1,26 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
-
 import { BaseModeler, BaseViewerOptions, UseBpmnJsModelerOptions, UseBpmnJsModelerResult } from "./Modeler.types";
+import { useBpmnJsViewer } from "../Viewer";
 
 export default <V extends BaseModeler, O extends BaseViewerOptions>({ factory, options }: UseBpmnJsModelerOptions<V, O>): UseBpmnJsModelerResult<V> => {
-    const [modeler, setModeler] = useState<V | null>(null);
-
-    const handleRef = useCallback((ref: HTMLElement | null) => {
-        if (modeler) {
-            if (ref) {
-                modeler.attachTo(ref)
-            } else {
-                modeler.detach();
-            }
-        } else if (ref) {
-            setModeler(factory({
-                ...options,
-                container: ref,
-            }));
-        }
-    }, [modeler, setModeler]);
-
-    useEffect(() => () => modeler?.destroy(), []);
+    const [handleRef, modeler] = useBpmnJsViewer<V, O>({
+        factory,
+        options
+    })
 
     return [handleRef, modeler];
 };
